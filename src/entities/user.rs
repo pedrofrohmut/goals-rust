d2bf547c4e93824891d9931e075224bb0ae001c8
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-05-24 09:14:16 -0300

    InvalidUserErr renamed

--- src/entities/user.rs
+++ src/entities/user.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use regex::Regex;
 
-use crate::errors::user_errors::InvalidUserErr;
+use crate::errors::user_errors::InvalidUserError;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CreateUserDto {
@@ -34,63 +34,63 @@ impl User {
         }
     }
 
-    pub fn validate_id(id: &str) -> Result<(), InvalidUserErr>
+    pub fn validate_id(id: &str) -> Result<(), InvalidUserError>
     {
         match Uuid::parse_str(id) {
-            Err(_) => Err(InvalidUserErr::new(Some(String::from("User id is not a valid UUID")))),
+            Err(_) => Err(InvalidUserError::new(Some(String::from("User id is not a valid UUID")))),
             Ok(_) => Ok(())
         }
     }
 
-    pub fn validate_name(name: &str) -> Result<(), InvalidUserErr>
+    pub fn validate_name(name: &str) -> Result<(), InvalidUserError>
     {
         if name.is_empty() {
-            return Err(InvalidUserErr::new(Some(String::from("User name is required and cannot be blank"))));
+            return Err(InvalidUserError::new(Some(String::from("User name is required and cannot be blank"))));
         }
         if name.len() < 5 || name.len() > 120 {
-            return Err(InvalidUserErr::new(Some(String::from("User name must be between 5 and 120 characters long"))));
+            return Err(InvalidUserError::new(Some(String::from("User name must be between 5 and 120 characters long"))));
         }
         Ok(())
     }
 
-    pub fn validate_email(email: &str) -> Result<(), InvalidUserErr>
+    pub fn validate_email(email: &str) -> Result<(), InvalidUserError>
     {
         if email.is_empty() {
-            return Err(InvalidUserErr::new(Some(String::from("User email is required and cannot be empty"))));
+            return Err(InvalidUserError::new(Some(String::from("User email is required and cannot be empty"))));
         }
         let pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
         let regex = Regex::new(pattern).unwrap();
         if ! regex.is_match(email) {
-            return Err(InvalidUserErr::new(Some(String::from("User email is not in a valid format"))));
+            return Err(InvalidUserError::new(Some(String::from("User email is not in a valid format"))));
         }
         Ok(())
     }
 
-    pub fn validate_password(password: &str) -> Result<(), InvalidUserErr>
+    pub fn validate_password(password: &str) -> Result<(), InvalidUserError>
     {
         if password.is_empty() {
-            return Err(InvalidUserErr::new(Some(String::from("User password is required and cannot be empty"))));
+            return Err(InvalidUserError::new(Some(String::from("User password is required and cannot be empty"))));
         }
         if password.len() < 3 || password.len() > 32 {
-            return Err(InvalidUserErr::new(Some(String::from("User password must be betweeen 3 and 32 characters long"))));
+            return Err(InvalidUserError::new(Some(String::from("User password must be betweeen 3 and 32 characters long"))));
         }
         Ok(())
     }
 
-    pub fn validate_phone(phone: &str) -> Result<(), InvalidUserErr>
+    pub fn validate_phone(phone: &str) -> Result<(), InvalidUserError>
     {
         if phone.is_empty() {
-            return Err(InvalidUserErr::new(Some(String::from("User phone is required and cannot be empty"))));
+            return Err(InvalidUserError::new(Some(String::from("User phone is required and cannot be empty"))));
         }
         let pattern = r"^\d{3}-\d{3}-\d{4}$";
         let regex = Regex::new(pattern).unwrap();
         if ! regex.is_match(phone) {
-            return Err(InvalidUserErr::new(Some(String::from("User phone is not in a valid phone pattern"))));
+            return Err(InvalidUserError::new(Some(String::from("User phone is not in a valid phone pattern"))));
         }
         Ok(())
     }
 
-    pub fn set_name(&mut self, name: String) -> Result<(), InvalidUserErr>
+    pub fn set_name(&mut self, name: String) -> Result<(), InvalidUserError>
     {
         match User::validate_name(&name) {
             Err(err) => Err(err),
@@ -101,7 +101,7 @@ impl User {
         }
     }
 
-    pub fn set_email(&mut self, email: String) -> Result<(), InvalidUserErr>
+    pub fn set_email(&mut self, email: String) -> Result<(), InvalidUserError>
     {
         match User::validate_email(&email) {
             Err(err) => Err(err),
@@ -112,7 +112,7 @@ impl User {
         }
     }
 
-    pub fn set_password(&mut self, password: String) -> Result<(), InvalidUserErr>
+    pub fn set_password(&mut self, password: String) -> Result<(), InvalidUserError>
     {
         match User::validate_password(&password) {
             Err(err) => Err(err),
@@ -123,7 +123,7 @@ impl User {
         }
     }
 
-    pub fn set_phone(&mut self, phone: String) -> Result<(), InvalidUserErr>
+    pub fn set_phone(&mut self, phone: String) -> Result<(), InvalidUserError>
     {
         match User::validate_phone(&phone) {
             Err(err) => Err(err),
@@ -142,7 +142,7 @@ impl User {
 
     pub fn get_phone(&self) -> String { self.phone.clone() }
 
-    pub fn from_create_user_dto(create_user: CreateUserDto) -> Result<User, InvalidUserErr>
+    pub fn from_create_user_dto(create_user: CreateUserDto) -> Result<User, InvalidUserError>
     {
         let mut user = User::new();
         if let Err(err) = user.set_name(create_user.name) {
--- src/errors/user_errors.rs
+++ src/errors/user_errors.rs
@@ -1,23 +1,23 @@
 use std::{fmt::{Display, self}, error::Error};
 
 #[derive(Debug)]
-pub struct InvalidUserErr(String);
+pub struct InvalidUserError(String);
 
-impl InvalidUserErr {
-    pub fn new(message: Option<String>) -> InvalidUserErr
+impl InvalidUserError {
+    pub fn new(message: Option<String>) -> InvalidUserError
     {
         match message {
-            Some(msg) => InvalidUserErr(msg),
-            None => InvalidUserErr("Err: User is invalid".into()),
+            Some(msg) => InvalidUserError(msg),
+            None => InvalidUserError("Err: User is invalid".into()),
         }
     }
 }
 
-impl Display for InvalidUserErr {
+impl Display for InvalidUserError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
     {
         write!(f, "{}", self.0)
     }
 }
 
-impl Error for InvalidUserErr {}
+impl Error for InvalidUserError {}
